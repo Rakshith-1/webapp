c63977dd53dcda28420814ec2352aac258352b4a
techieinterest <135801173+techieinterest@users.noreply.github.com>
2023-12-30 12:23:27 +0530

    Update pushhelmpackfe.sh

--- terraform-scripts/frontend-chart/pushhelmpackfe.sh
+++ terraform-scripts/frontend-chart/pushhelmpackfe.sh
@@ -1,6 +1,6 @@
-aws s3api create-bucket --bucket frontendchartrakshit
-helm s3 init s3://frontendchartrakshit/frontend-foldername
+sudo -u root aws s3api create-bucket --bucket frontendchartrakshit
+sudo -u root helm s3 init s3://frontendchartrakshit/frontend-foldername
 sleep 5
-helm package .
-helm repo add frontendchartrakshit s3://frontendchartrakshit/frontend-foldername
-helm s3 push frontend-0.1.0.tgz frontendchartrakshit
+sudo -u root helm package .
+sudo -u root helm repo add frontendchartrakshit s3://frontendchartrakshit/frontend-foldername
+sudo -u root helm s3 push frontend-0.1.0.tgz frontendchartrakshit
