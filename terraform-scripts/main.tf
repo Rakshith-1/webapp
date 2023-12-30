2a76ccb979d7b0f3225f6e2d970ab7981f5301fa
techieinterest <135801173+techieinterest@users.noreply.github.com>
2023-12-30 16:11:06 +0530

    Update main.tf

--- terraform-scripts/main.tf
+++ terraform-scripts/main.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-webapps-bucket"
     key            = "terraform.tfstate"
+    region         = "ap-south-1"
     encrypt        = true
   }
 }
