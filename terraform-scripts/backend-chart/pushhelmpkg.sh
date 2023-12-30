f39b0140a6f778d3d6680d6b17a526080967519a
techieinterest <135801173+techieinterest@users.noreply.github.com>
2023-12-30 12:23:57 +0530

    Update pushhelmpkg.sh

--- terraform-scripts/backend-chart/pushhelmpkg.sh
+++ terraform-scripts/backend-chart/pushhelmpkg.sh
@@ -1,6 +1,6 @@
-aws s3api create-bucket --bucket backendchartrakshit
-helm s3 init s3://backendchartrakshit/backend-foldername
+sudo -u root aws s3api create-bucket --bucket backendchartrakshit
+sudo -u root helm s3 init s3://backendchartrakshit/backend-foldername
 sleep 5
-helm package .
-helm repo add backendchartrakshit s3://backendchartrakshit/backend-foldername
-helm s3 push backend-0.1.0.tgz backendchartrakshit
+sudo -u root helm package .
+sudo -u root helm repo add backendchartrakshit s3://backendchartrakshit/backend-foldername
+sudo -u root helm s3 push backend-0.1.0.tgz backendchartrakshit
