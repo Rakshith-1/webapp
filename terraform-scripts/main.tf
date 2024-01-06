b891c883b7ba0d8adcf2aece28f3b3cd2f85cef9
techieinterest <135801173+techieinterest@users.noreply.github.com>
2024-01-06 12:16:42 +0530

    Update main.tf

--- terraform-scripts/main.tf
+++ terraform-scripts/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "var.aws_tfbucket"
+    bucket         = var.aws_tfbucket
     key            = "terraform.tfstate"
     region         = var.aws_region
     encrypt        = true
